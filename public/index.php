186c4298074019cd2321cbab3730777105957ba8
Michel - gaming laptop <michel-disbergen@hotmail.com>
2021-11-26 14:46:55 +0100

    template aangemaakt voor start project

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>template</title>
+    <?php include "../templates/head.php" ?>
+  </head>
+
+  <body>
+    <!-- start navbar -->
+
+    <div class="navbar">
+      <?php include "../templates/navbar.php";?>
+    </div>
+
+    <!-- end navbar -->
+    <!-- start main container -->
+
+    <div class="container">
+
+      <div></div>
+      <div></div>
+
+    </div>
+
+    <!-- end main container  -->
+    <!-- start footer -->
+
+    <div class="footer">
+      <?php include "../templates/footer.php";?>
+    </div>
+    <!-- end footer -->
+    
+  </body>
+
+</html>
