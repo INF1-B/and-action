8058512b7d7db36f4f52838b2e5598c4b494d7a1
Noizeisdead <noize829@gmail.com>
2021-11-29 15:28:15 +0100

    Setup for homepage

---
+++ public/homepage.php
@@ -0,0 +1,58 @@
+<?php
+  include '../src/database/credentials.php';
+  echo DB_HOST;
+
+
+    // $movie = array(
+    //     conor mcgegor = array(
+    //         $src => ''
+    //     ), 
+    // )
+
+//   printMovies(){
+//       foreach($movies as $movie){
+//           echo '<img class="movie" src="" alt="movie">';
+//           echo 'title';
+
+//       }
+//   }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>template</title>
+    <?php include "../templates/head.php" ?>
+  </head>
+
+  <body>
+    <!-- start navbar -->
+
+    <div class="navbar">
+      <?php include "../templates/navbar.php";?>
+    </div>
+
+    <!-- end navbar -->
+    <!-- start main container -->
+
+    <div class="container">
+    <h1><span>Search:</span> Documentary</h1>        
+      <div class="movies">
+        
+
+      </div>
+      <div></div>
+
+    </div>
+
+    <!-- end main container  -->
+    <!-- start footer -->
+
+    <div class="footer">
+      <?php include "../templates/footer.php";?>
+    </div>
+    <!-- end footer -->
+    
+  </body>
+
+</html>
