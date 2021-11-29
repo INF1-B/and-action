9917318080c7cb604ebe09f310bfb2307fd261b0
Michel - gaming laptop <michel-disbergen@hotmail.com>
2021-11-29 22:07:47 +0100

    made some small updates to directory my movies page

--- public/index.php
+++ public/index.php
@@ -19,11 +19,25 @@
     <!-- end navbar -->
     <!-- start main container -->
 
-    <div class="container">
-
-      <div></div>
-      <div></div>
-
+    <div class="container container-my-movie-director">
+      <h1>My Movies</h1>
+      <div class="movie-row"> 
+        <?php 
+          for ($i=0; $i < 5; $i++) { 
+            echo "<div class=\"movie\"> 
+            </div>";
+          }
+        ?>
+      </div>
+      <h1> Under review </h1>
+      <div class="movie-row"> 
+        <?php 
+          for ($i=0; $i < 2; $i++) { 
+            echo "<div class=\"movie\"> 
+            </div>";
+          }
+        ?>
+      </div>
     </div>
 
     <!-- end main container  -->
