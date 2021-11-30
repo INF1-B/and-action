a705b7956b98ad680d1dcedb350e4c467b227e26
Hajo300 <89904094+Hajo300@users.noreply.github.com>
2021-11-30 12:47:39 +0100

    removed footer

--- public/index.php
+++ public/index.php
@@ -28,12 +28,6 @@
     </div>
 
     <!-- end main container  -->
-    <!-- start footer -->
-
-    <div class="footer">
-      <?php include "../templates/footer.php";?>
-    </div>
-    <!-- end footer -->
     
   </body>
 
