d3bd948b4619a6cde413640fff6e49ab1c320b95
Noizeisdead <noize829@gmail.com>
2021-12-03 13:39:16 +0100

    Added div, specified styling

--- public/dissaprovePage.php
+++ public/dissaprovePage.php
@@ -17,22 +17,23 @@
     <!-- end navbar -->
     <!-- start main container -->
 
-
-    <div class="container">
+    <div class="dissaprovePageContainer">
+      <div class="container">
         <div class="content">
-            <span>Are you sure ?</span>
-            <p>You are about to disapprove: <a href="">Ron's gone wrong</a>
-            <div class="formWrapper">
-                <form action="" methode="POST">
-                    <label for="reasonDissaprove">Reason</label>
-                    <textarea id="reasonDissaprove" name="reason" rows="6" placeholder="Type here your reason why the movie is not approved"></textarea>
-                    <div class="submitButtons">
-                        <input type="submit" name="approve" id="approve" value="Approve">
-                        <input type="submit" name="cancel" id="cancel" value="Cancel">
-                    </div>
-                </form>
-            </div>
+          <span>Are you sure ?</span>
+          <p>You are about to disapprove: <a href="#">Ron's gone wrong</a>
+          <div class="formWrapper">
+            <form action="" methode="POST">
+              <label for="reasonDissaprove">Reason</label>
+              <textarea id="reasonDissaprove" name="reason" rows="6" placeholder="Type here your reason why the movie is not approved"></textarea>
+              <div class="submitButtons">
+                <input type="submit" name="approve" id="approve" value="Approve">
+                <input type="submit" name="cancel" id="cancel" value="Cancel">
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
 
     <!-- end main container  -->
