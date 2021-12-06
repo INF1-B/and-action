279020a30e7a47255ad8650adea7442217000932
= <=>
2021-12-06 16:24:40 +0100

    updated login page to be a bit more responsive

--- public/login.php
+++ public/login.php
@@ -1,34 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-    <title>Log in</title>
-    <?php include "../templates/head.php" ?>
-    <link rel="stylesheet" href="../public/assets/css/login.css">
+<head>
+  <title>Log in</title>
+  <?php include "../templates/head.php" ?>
+  <link rel="stylesheet" href="../public/assets/css/login.css">
 
-  <body>
-  
-   <div class="container">
-    <div class="row">
-    
+<body>
+
+  <div class="container container-login">
     <div class="upper">
-      <h1>Login</h1>
-      <img class="logo" src="../public/assets/img/logo_login.png" alt="logo">
+      <div>
+        <h1>Login</h1>
+      </div>
+      <div>
+        <img class="logo" src="../public/assets/img/logo_login.png" alt="logo">
+      </div>
     </div>
-      <form method="post">
-        <input class="input" type="text" name="email" id="email" placeholder="Email address"><br>
-        <input class="input" type="text" name="password" id="password" placeholder="Password"><br>
-          <div class="SignedIn">
-          <input class="checkmark" type="checkbox" name="signed" id="signed">Keep me signed in<br>
-          </div>
-        <input class="button" type="submit" name="submit" value="login"><br>
-        <div class="SignUp">Don't have an account?<div class="signUp"><a href="#"> Sign up </a></div></div>
-       </form>
-
-
-
-    </div>
-   </div> 
-  </body>
+    <form method="post">
+      <div class="input-login-text">
+        <input class="input" type="text" name="email" id="email" placeholder="Email address">
+        <input class="input" type="text" name="password" id="password" placeholder="Password">
+      </div>
+      <div class="checkbox-form">
+        <input type="checkbox" name="signed" id="signed">
+        <label for="signed"> Keep me signed in </label>
+      </div>
+      <div class="submit-form">
+        <input type="submit" name="submit" value="login">
+      </div>
+    </form>
+    <p class="no-account">
+      Don't have an account? 
+      <a class="sign-up" href="#"> Sign up</a>
+    </p>
+  </div>
+</body>
 
 </html>
