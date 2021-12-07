46c0fa1e60e3f7df52b5b9e0b1d9f343ff4c3c7b
Moniquesbg <moniquesbg@gmail.com>
2021-12-07 13:58:25 +0100

    Created homepage

---
+++ public/homepage.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>homepage</title>
+    <?php include "../templates/head.php" ?>
+    <link rel="stylesheet" type="text/css" href="../public/assets/css/homepage.css">
+</head>
+
+<body>
+
+    <div class="navbar">
+        <?php include "../templates/navbar.php";?>
+    </div>
+
+
+    <div class="container">
+
+    <!--Recently watched -->
+        <div class="upper">
+            <h2>Recently watched</h2>
+            <input class="button" type="button" name="filter" value="filter">
+        </div>
+
+        <div class="movies">
+            <div class="movie">
+                <div class="movie-1">
+                </div>
+                <p>Scary movie</p>
+            </div>
+
+            <div class="movie">
+                <div class="movie-1">
+                </div>
+                <p>Scary movie</p>
+            </div>
+
+            <div class="movie">
+                <div class="movie-1">
+                </div>
+                <p>Scary movie</p>
+            </div>
+        </div>
+
+
+<!--Documentary-->
+        <div class="documentary">
+            <h2>Documentary</h2>
+        </div>
+
+        <div class="movies">
+            <div class="movie">
+                <div class="movie-1">
+                </div>
+                <p>Scary movie</p>
+            </div>
+
+            <div class="movie">
+                <div class="movie-1">
+                </div>
+                <p>Scary movie</p>
+            </div>
+
+            <div class="movie">
+                <div class="movie-1">
+                </div>
+                <p>Scary movie</p>
+            </div>
+
+            <div class="movie">
+                <div class="movie-1">
+                </div>
+                <p>Scary movie</p>
+            </div>
+
+            <div class="movie">
+                <div class="movie-1">
+                </div>
+                <p>Scary movie</p>
+            </div>
+
+            <div class="movie">
+                <div class="movie-1">
+                </div>
+                <p>Scary movie</p>
+            </div>
+        </div>
+
+
+    </div>
+
+    <!-- end main container  -->
+
+</body>
+
+</html>
