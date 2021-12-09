dae0f2292b7df9ffeb5140b43cc2a7ce71bb86a4
Michel - gaming laptop <michel-disbergen@hotmail.com>
2021-12-09 15:34:55 +0100

    updated upload.php with examples on how to use different functions, and related to uploading and filtering user input

--- utils/upload.php
+++ utils/upload.php
@@ -5,18 +5,44 @@ This file will contain all functions related to uploading of movies/thumbnails a
 */
 
 // filters text, if not empty it will return the value filtered, if empty it will return false
-function filterInputText($value){
-  $value = !empty($value) ? htmlspecialchars($value) : "ERROR: value is empty, cannot filter!";
+function filterInputTextGeneral($value){
+  $value = !empty($value) ? htmlspecialchars($value) : "ERROR: input value is empty, cannot filter!";
   return $value;
 }
 
-// returns true if an email address is valid, if not it will return false
+/* checks if a variable is empty, if not it filters the value and returns it
+*
+* Example : $filteredValue = filterInputGet($_GET["id"], "id");
+*
+*/
+// to be tested
+function filterInputGet($getVariable, $parameterName){
+  $filteredValue = !empty($getVariable) ? FILTER_INPUT(INPUT_GET, $parameterName, FILTER_SANITIZE_SPECIAL_CHARS) : "ERROR, unable to filter " . $parameterName;
+  return $filteredValue;
+}
+
+/* same as filterInputGet, but with a $_POST variable
+*
+* Example : $filteredValue = filterInputPost($_POST["id"], "id");
+*
+*/
+// to be tested
+function filterInputPost($postVariable, $parameterName){
+  $filteredValue = !empty($postVariable) ? FILTER_INPUT(INPUT_POST, $parameterName, FILTER_SANITIZE_SPECIAL_CHARS) : "ERROR, unable to filter " . $parameterName;
+  return $filteredValue;
+}
+
+// returns true if an email address is valid, if not it will return an error message
 function filterEmail($email){
   $email = filter_var($email, FILTER_VALIDATE_EMAIL) ? true : "ERROR: Not an valid email!";
   return $email;
 }
 
-// returns true if an file is smaller then a certain amount, $file is the image/video and the $size is in kb
+/* returns true if an file is smaller then a certain amount, $file is the image/video and the $size is in kb
+*
+* Example : filterInputFileSize($_FILES["uploadedFileParameter"]["name"], 500); // allowed file size of the uploaded file/video is 500kb
+*
+*/
 // to be tested
 function filterInputFileSize($file, $size){
   $size = $size * 1000; // converts it to KB
@@ -26,7 +52,11 @@ function filterInputFileSize($file, $size){
   return "ERROR: file size is to big! it must be a max of " . $size . " KB";
 }
 
-// returns true if a file is of a specific MIME type, returns false if it is not equal to the specific MIME type
+/* returns true if a file is of a specific MIME type, returns false if it is not equal to the specific MIME type
+*
+* Example: filterFileMimeType($_FILES["uploadedFileParameter"]["tmp_name"], array("image/gif", "image/jpg", "image/jpeg"));
+*
+*/
 // to be tested
 function filterFileMimeType($file, $allowedMimeTypes){
   $uploadedMimeType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $file);
@@ -36,7 +66,12 @@ function filterFileMimeType($file, $allowedMimeTypes){
   return "ERROR: MIME TYPE not supported";
 }
 
-// returns true if a file can be uploaded, and a error string if the uploaded file can't be moved 
+/* returns true if a file can be uploaded, then uploads a file, and a error string if the uploaded file can't be moved
+*
+* Example 1: moveUploadedFile("upload/images", $_FILES["uploadedFileParameter"]["tmp_name"], $_FILES["uploadedFileParameter"]["name"]);
+* Example 2: moveUploadedFile("upload/images", $_FILES["uploadedFileParameter"]["tmp_name"], "pot-pindakaas.jpg");
+*
+*/
 // to be tested
 function moveUploadedFile($destination, $tmpFileName, $fileName){
   if (!file_exists($destination . "/" . $fileName)) {
@@ -47,7 +82,11 @@ function moveUploadedFile($destination, $tmpFileName, $fileName){
   return "ERROR: Unable to move the uploaded file to the directory!";
 }
 
-// returns true if the length of a movie is less then 60 characters, if not it will return an error message
+/* returns true if the length of a movie is less then 60 characters, if not it will return an error message
+*
+* Example : filterUploadFileLength($_FILES["uploadedFileParameter"]["name"], 50) // if the name of the file is less or equal to 50, it will return true.
+*
+*/
 // to be tested
 function filterUploadFileLength($fileName, $allowedLength){
   if (strlen($fileName) <= $allowedLength){
