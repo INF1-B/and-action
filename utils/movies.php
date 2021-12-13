5b8828e8c53b5d16adb5153bab1e9ad7e3ee1b3f
Noizeisdead <noize829@gmail.com>
2021-12-13 13:37:58 +0100

    Added file for functions movie upload

---
+++ utils/movies.php
@@ -0,0 +1,8 @@
+<?php 
+//Een functie maken die ervoor zorgt dat een film succesvol geupload kan worden - hierbij moet het ook in de database geupdatet worden (tabel film, genre_film, film_kijkwijzer_geschiktheid)
+
+
+
+
+
+?>
