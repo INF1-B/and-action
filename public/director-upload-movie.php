061211c7fda90f40b139508e26fb130c46ac46dc
Sir-Sunshine <serhan.gunes@student.nhlstenden.com>
2021-12-13 11:40:48 +0100

    still some css left but most of the bugs are fix

--- public/director-upload-movie.php
+++ public/director-upload-movie.php
@@ -28,22 +28,22 @@
           <p class="spacetextupload">Category</p>
             <div class="multi-selector">
               <div class="select-field">
-              <input type="text" name="" placeholder="Choose Category" id="" class="input-selector" disabled>
+              <input type="text" name="Category" placeholder="Choose Category" id="Category" class="input-selector" disabled>
               <span class="down-arrow">&blacktriangledown;</span>
               </div>
               <div class="list">
-                <label for="Action" class="task"><input type="checkbox" name="Action" id="Action">Action</label>
-                <label for="Comedy" class="task"><input type="checkbox" name="Comedy" id="Comedy">Comedy</label>
-                <label for="Drama" class="task"><input type="checkbox" name="Drama" id="Drama">Drama</label>
-                <label for="Documentary" class="task"><input type="checkbox" name="Documentary" id="Documentary">Documentary</label>
-                <label for="Fantasy" class="task"><input type="checkbox" name="Fantasy" id="Fantasy">Fantasy</label>
-                <label for="Horror" class="task"><input type="checkbox" name="Horror" id="Horror">Horror</label>
-                <label for="Mystery" class="task"><input type="checkbox" name="Mystery" id="Mystery">Mystery</label>
-                <label for="Romance" class="task"><input type="checkbox" name="Romance" id="Romance">Romance</label>
-                <label for="Sci-if" class="task"><input type="checkbox" name="Sci-if" id="Sci-if">Sci-if</label>
-                <label for="Thriller" class="task"><input type="checkbox" name="Thriller" id="Thriller">Thriller</label>
-                <label for="Western" class="task"><input type="checkbox" name="Western" id="Western">Western</label>
-                <label for="Other" class="task"><input type="checkbox" name="Other" id="Other">Other</label>
+                <label for="Action" class="task"><input type="checkbox" name="Action" id="Action"> Action</label>
+                <label for="Comedy" class="task"><input type="checkbox" name="Comedy" id="Comedy"> Comedy</label>
+                <label for="Drama" class="task"><input type="checkbox" name="Drama" id="Drama"> Drama</label>
+                <label for="Documentary" class="task"><input type="checkbox" name="Documentary" id="Documentary"> Documentary</label>
+                <label for="Fantasy" class="task"><input type="checkbox" name="Fantasy" id="Fantasy"> Fantasy</label>
+                <label for="Horror" class="task"><input type="checkbox" name="Horror" id="Horror"> Horror</label>
+                <label for="Mystery" class="task"><input type="checkbox" name="Mystery" id="Mystery"> Mystery</label>
+                <label for="Romance" class="task"><input type="checkbox" name="Romance" id="Romance"> Romance</label>
+                <label for="Sci-if" class="task"><input type="checkbox" name="Sci-if" id="Sci-if"> Sci-if</label>
+                <label for="Thriller" class="task"><input type="checkbox" name="Thriller" id="Thriller"> Thriller</label>
+                <label for="Western" class="task"><input type="checkbox" name="Western" id="Western"> Western</label>
+                <label for="Other" class="task"><input type="checkbox" name="Other" id="Other"> Other</label>
               </div>
             </div>
           </div>
@@ -61,18 +61,18 @@
             </select> 
           </div>  
           <div class="spaceupload">
-          <p class="spacetextupload">Kijkwijzer</p>
+          <p class="spacetextupload">Filmguide</p>
             <div class="multi-selector">
-              <div class="select-field">
-              <input type="text" name="" placeholder="Choose Category" id="" class="input-selector" disabled>
-              <span class="down-arrow">&blacktriangledown;</span>
+              <div class="select-fieldFilmguide">
+                <input type="text" name="Filmguide" placeholder="Choose Film guide" id="Filmguide" class="input-selector" disabled>
+                <span class="down-arrow" id="downArrow2">&blacktriangledown;</span>
               </div>
-              <div class="list">
-                <label for="Voilence" class="task"><input type="checkbox" name="Voilence" id="Voilence">Voilence</label>
-                <label for="Sex" class="task"><input type="checkbox" name="Sex" id="Sex">Sex</label>
-                <label for="Discrimnation" class="task"><input type="checkbox" name="Discrimnation" id="Discrimnation">Discrimnation</label>
-                <label for="Fear" class="task"><input type="checkbox" name="Fear" id="Fear">Fear</label>
-                <label for="Foul launguage" class="task"><input type="checkbox" name="Foul launguage" id="Foul launguage">Foul launguage</label>
+              <div class="listFilmguide">
+                <label for="Voilence" class="task"><input type="checkbox" name="Voilence" id="Voilence"> Voilence</label>
+                <label for="Sex" class="task"><input type="checkbox" name="Sex" id="Sex"> Sex</label>
+                <label for="Discrimnation" class="task"><input type="checkbox" name="Discrimnation" id="Discrimnation"> Discrimnation</label>
+                <label for="Fear" class="task"><input type="checkbox" name="Fear" id="Fear"> Fear</label>
+                <label for="Foul-launguage" class="task"><input type="checkbox" name="Foul launguage" id="Foul-launguage"> Foul launguage</label>
               </div>
             </div>
           </div>
@@ -106,6 +106,11 @@
     document.querySelector('.select-field').addEventListener('click',()=>{
         document.querySelector('.list').classList.toggle('show');
         document.querySelector('.down-arrow').classList.toggle('rotate180'); });
+    document.querySelector('.select-fieldFilmguide').addEventListener('click',()=>{
+        document.querySelector('.listFilmguide').classList.toggle('show');
+        document.getElementById('downArrow2').classList.toggle('rotate180'); 
+      });  
+
    </script>
    <!-- end of script -->
   </body>
