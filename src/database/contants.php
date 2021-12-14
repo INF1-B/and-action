4ad2137cd4590c4e8c14149b5d4d08da8d0a64c3
Moniquesbg <moniquesbg@gmail.com>
2021-12-14 09:32:15 +0100

    Mime types and extensions

---
+++ src/database/contants.php
@@ -0,0 +1,12 @@
+<?php
+
+//	JPG: image/jpeg, JPEG: image/jpeg , PNG: image/png, GIF:image/gif, MP4:video/mp4
+
+$mimeTypes = array("image/jpeg", "image/png", "image/gif");
+
+$extensions = array(".jpeg",".jpg",".png",".gif");
+
+$mimeTypesVideo = array ("video/mp4","video/mp3");
+
+$extensionsVideo = array (".mp4",".mp3");
+?>
