3f4eb7bc39644dd9436a23fb8e4c71d42866a3ec
Noizeisdead <noize829@gmail.com>
2021-12-15 12:11:08 +0100

    Added likeMovie() and getComment()

--- utils/movies.php
+++ utils/movies.php
@@ -1,7 +1,23 @@
 <?php 
-//Een functie maken die ervoor zorgt dat een film succesvol geupload kan worden - hierbij moet het ook in de database geupdatet worden (tabel film, genre_film, film_kijkwijzer_geschiktheid)
 
 
+
+
+
+//  Function that adds to tumb_up table when liking a video. It will check if the user and film like combination does not already exist. If it exists it will not add otherwise it will.
+function likeMovie($userId, $filmId){
+    echo executeQuery("INSERT IGNORE INTO thumb_up (gebruiker_id, film_id) VALUES (?, ?);","ii",array($userId, $filmId));
+    $message = "You have liked this movie";
+    return $message;
+}
+
+// Function that will add comment given on a movie to the database table 'commentaar'.
+function getComment($filmId, $userId, $message){
+    $date = date('Y-m-d H:i:s');
+    executeQuery("INSERT INTO commentaar (film_id, gebruiker_id, bericht, tijdsstempel) VALUES (?, ?, ?, ?)", "iiss", array($filmId, $userId, $message , $date));
+    $message = "Added comment";
+    return $message;
+}
 
 
 
