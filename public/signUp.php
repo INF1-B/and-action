ebeacce42f97cae2baf2cfbf99a39abcb95ed368
Michel - gaming laptop <michel-disbergen@hotmail.com>
2021-12-15 08:38:34 +0100

    added functionality for filtering password, and prefill email and username field once registration is unsuccesfull

--- public/signUp.php
+++ public/signUp.php
@@ -9,36 +9,41 @@ $message = "
   Note that signing up can take some time. You will get a message once registration is succesfull
 </p>";
 
+
 if (isset($_POST["submit"])) { 
   // filter all user values
   $input["username"] = filterInputPost($_POST["username"], "username");
   $input["password"] = filterInputPost($_POST["password"], "password");
-  $input["email"] = filterEmail($_POST["email"]);
   $input["subscription"] = filterInputPost($_POST["subscription"], "subscription");
+  $input["email"] = filterEmail($_POST["email"]);
   $input["email"] = $input["email"] ? filterInputPost($_POST["email"], "email") : false;
-
-  // check if any values were false, if so return an error, else insert the new user 
-  if (in_array(false, $input)) {
-    $message = "
-    <p class=\"register-error\">
-      Please double check if all fields were filled in, and if your email is a legitimate email address!
-    </p>";
-  } else {
-
-    if (executeQuery("INSERT INTO gebruiker (rol_id, abonnement_id, geverifieerd, ingelogd, gebruikersnaam, wachtwoord, email) 
-                        VALUES (?, ?, ?, ?, ?, ?, ?)", 
-                       "iiiisss",
-                       array(3,  $input["subscription"], 0, 0, $input["username"], generateHash($input["password"]), $input["email"]))) {
-      $message = "
-      <p class=\"register-success\">
-        You have been registered! Please log in at the <a href=\"login.php\"> Login page </a>
-      </p>";
-      } else {
+  if (filterPassword($input["password"])){
+    if (in_array(false, $input)) {
       $message = "
       <p class=\"register-error\">
-        ERROR: Duplicate email. Please pick another email to register!
+        Please double check if all fields were filled in, and if your email is a legitimate email address!
       </p>";
+    } else {
+      if (executeQuery("INSERT INTO gebruiker (rol_id, abonnement_id, geverifieerd, ingelogd, gebruikersnaam, wachtwoord, email) 
+                          VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                         "iiiisss",
+                         array(3,  $input["subscription"], 0, 0, $input["username"], generateHash($input["password"]), $input["email"]))) {
+        $message = "
+        <p class=\"register-success\">
+          You have been registered! Please log in at the <a href=\"login.php\"> Login page </a>
+        </p>";
+        } else {
+        $message = "
+        <p class=\"register-error\">
+          ERROR: Duplicate email. Please pick another email to register!
+        </p>";
+      }
     }
+  } else {
+    $message = "
+    <p class=\"register-error\">
+      Your password must contain atleast 8 characters, containing 1 special character, 1 uppercase character and 1 number!.
+    </p>";
   }
 }
 
@@ -50,7 +55,7 @@ if (isset($_POST["submit"])) {
 <head>
   <title>Sign up director</title>
   <?php
-  include "../templates/head.php";
+    include "../templates/head.php";
   ?>
   <link rel="stylesheet" href="assets/css/signup.css">
 </head>
@@ -66,24 +71,30 @@ if (isset($_POST["submit"])) {
         <img class="logo" src="../public/assets/img/logo_login.png" alt="logo">
       </div>
     </div>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
       <div>
         <label for="email">Email address</label>
-        <input type="text" name="email" id="email" placeholder="Type here your email address">
+        <input
+          value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' // if form is filled in, prefill same values ?>"
+          type="text" name="email" id="email" placeholder="Type here your email address">
         <label for="username">Username</label>
-        <input type="text" name="username" id="username" placeholder="Type here your username">
+        <input
+          value="<?php echo isset($_POST['username']) ? $_POST['username'] : '' // if form is filled in, prefill same values ?>"
+          type="text" name="username" id="username" placeholder="Type here your username">
         <label for="password">Password</label>
         <input type="password" name="password" id="password" placeholder="Type here your password">
       </div>
-
       <div>
         <label for="subscription">Subscription</label>
         <select id="subscription" class="subscription" name="subscription">
+
+          <!-- start subscription types, people can have this field prefilled from the index.php page - subscriptions -->
           <?php 
             $subscriptions = getTableRecords("SELECT id, naam FROM abonnement");
             if (!isset($_GET["subscription"])) {
-              echo "<option value=\"0\" selected> Please pick your subscription </option>";
-            }
+              echo "<option value=\"0\" selected hidden> Please pick your subscription </option>";
+            } 
             
             foreach ($subscriptions as $key => $subscription) {
               if (isset($_GET["subscription"]) && strtolower($_GET["subscription"]) == strtolower($subscription["naam"])) {
@@ -92,14 +103,16 @@ if (isset($_POST["submit"])) {
                 echo "<option value=" . $subscription["id"] . " >" . $subscription["naam"] . "</option>";
               }
             }
-
           ?>
+          <!-- end subscription types -->
+
         </select>
       </div>
       <div class="submit-form">
         <input type="submit" name="submit" value="Sign up">
       </div>
     </form>
+
     <?php
       echo $message;
     ?>
--- utils/filter.php
+++ utils/filter.php
@@ -15,30 +15,33 @@ function filterInputTextGeneral($value){
 * Example : $filteredValue = filterInputGet($_GET["id"], "id");
 *
 */
-// to be tested
 function filterInputGet($getVariable, $parameterName){
   $filteredValue = !empty($getVariable) ? FILTER_INPUT(INPUT_GET, $parameterName, FILTER_SANITIZE_SPECIAL_CHARS) : false;
   return $filteredValue;
 }
 
-/* same as filterInputGet, but with a $_POST variable
+/* filterInputPost(): same as filterInputGet, but with a $_POST variable
 *
 * Example : $filteredValue = filterInputPost($_POST["id"], "id");
 *
 */
-// to be tested
 function filterInputPost($postVariable, $parameterName){
   $filteredValue = !empty($postVariable) ? FILTER_INPUT(INPUT_POST, $parameterName, FILTER_SANITIZE_SPECIAL_CHARS) : false;
   return $filteredValue;
 }
 
-// returns true if an email address is valid, if not it will return an error message
+/* filterEmail(): returns true if an email address is valid, if not it will return an error message
+*
+* Example 1 : filterEmail("michel.disbergen@test.com"); // returns true
+* Example 2 : filterEmail("michel.disbergen@test"); // returns false
+*
+*/
 function filterEmail($email){
   $email = filter_var($email, FILTER_VALIDATE_EMAIL) ? true : false;
   return $email;
 }
 
-/* returns true if an file is smaller then a certain amount, $file is the image/video and the $size is in kb
+/* filterInputFileSize(): returns true if an file is smaller then a certain amount, $file is the image/video and the $size is in kb
 *
 * Example : filterInputFileSize($_FILES["uploadedFileParameter"]["name"], 500); // allowed file size of the uploaded file/video is 500kb
 *
@@ -52,7 +55,7 @@ function filterInputFileSize($file, $size){
   return "ERROR: file size is to big! it must be a max of " . $size . " KB";
 }
 
-/* returns true if a file is of a specific MIME type, returns false if it is not equal to the specific MIME type
+/* filterFileMimeTypes(): returns true if a file is of a specific MIME type, returns false if it is not equal to the specific MIME type
 *
 * Example: filterFileMimeType($_FILES["uploadedFileParameter"]["tmp_name"], array("image/gif", "image/jpg", "image/jpeg"));
 *
@@ -66,13 +69,12 @@ function filterFileMimeType($file, $allowedMimeTypes){
   return "ERROR: MIME TYPE not supported";
 }
 
-/* returns true if a file can be uploaded, then uploads a file, and a error string if the uploaded file can't be moved
+/* moveUploadedFile(): returns true if a file can be uploaded, then uploads a file, and a error string if the uploaded file can't be moved
 *
 * Example 1: moveUploadedFile("upload/images", $_FILES["uploadedFileParameter"]["tmp_name"], $_FILES["uploadedFileParameter"]["name"]);
 * Example 2: moveUploadedFile("upload/images", $_FILES["uploadedFileParameter"]["tmp_name"], "pot-pindakaas.jpg");
 *
 */
-// to be tested
 function moveUploadedFile($destination, $tmpFileName, $fileName){
   if (!file_exists($destination . "/" . $fileName)) {
     if (move_uploaded_file($tmpFileName, $destination . "/" . $fileName)){
@@ -82,12 +84,11 @@ function moveUploadedFile($destination, $tmpFileName, $fileName){
   return "ERROR: Unable to move the uploaded file to the directory!";
 }
 
-/* returns true if the length of a movie is less then 60 characters, if not it will return an error message
+/* filterUploadFileLength(): returns true if the length of a movie is less then 60 characters, if not it will return an error message
 *
 * Example : filterUploadFileLength($_FILES["uploadedFileParameter"]["name"], 50) // if the name of the file is less or equal to 50, it will return true.
 *
 */
-// to be tested
 function filterUploadFileLength($fileName, $allowedLength){
   if (strlen($fileName) <= $allowedLength){
     return true;
@@ -95,7 +96,7 @@ function filterUploadFileLength($fileName, $allowedLength){
   return "ERROR: the name of the uploaded file contains to many characters!";
 }
 
-/* returns true if the resolution of an image is $x x $y (e.g. : 400x600), if it is not that image size it will return an error message
+/* filterImageResolution(): returns true if the resolution of an image is $x x $y (e.g. : 400x600), if it is not that image size it will return an error message
 *
 * Example 1 : filterImageResolution('https://media.geeksforgeeks.org/wp-content/uploads/geeksforgeeks-13.png', 667, 184); // return true
 * Example 2 : filterImageResolution('https://media.geeksforgeeks.org/wp-content/uploads/geeksforgeeks-13.png', 667, 185); // return error message
@@ -113,4 +114,24 @@ function filterImageResolution($image, $x, $y){
   return "ERROR: The size of the image does not fit the specified x and y axes, these are " . $x . " x " . $y . " yours are $imageX x $imageY";
 }
 
+/* filterPassword(): checks whether the password contains the minimum of 8 characters in a password, and if the password matches the specified rules, the rules are noted underneath
+*
+* 8 characters minimum
+* 1 number (minimum)
+* 1 special character (minimum)
+* 1 uppercase character (minimum)
+*
+*/
+function filterPassword($password){
+  if (strlen($password) >= 8) { // minimum of 8 characters long
+    if (preg_match('/[A-Z]/', $password)) { // contains atleast 1 uppercase letter
+      if (preg_match('/[0-9]/', $password)) { // contains atleast 1 number
+        if (preg_match('/[^a-zA-Z\d]/', $password)) { // contains atleast 1 special character
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
 ?>
