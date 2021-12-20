1765343c427b6a94a4b71315ba29a073ffc066f7
Noizeisdead <noize829@gmail.com>
2021-12-20 12:39:37 +0100

    Removed error message

--- utils/movies.php
+++ utils/movies.php
@@ -1,8 +1,5 @@
 <?php 
 require "database.php";
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 // Upload movie function
 
 // example: uploadMovie(1, "title", "asdfg", "p" , 1, "asdfg", 1, 3);
