78b6cbb57758bd2ceb3f69ece9489f03f8e1f09f
Noizeisdead <noize829@gmail.com>
2021-12-22 08:14:01 +0100

    removed ['id'] from genreinput and filmguideinput

--- utils/movies.php
+++ utils/movies.php
@@ -5,24 +5,21 @@ require "database.php";
 // example: uploadMovie(1, "title", "asdfg", "p" , 1, "asdfg", 1, 3);
 function uploadMovie($userId, $title, $path, $thumbnailPath, $description, $ageRating, $filmGuides, $genres){
     $title = filterInputPost($_POST['MovieTitle'], 'MovieTitle');
-    $genre = filterInputPost($_POST['Category'], 'Category'); 
     $ageRating = filterInputPost($_POST['AgeRating'], 'AgeRating');
-    $filmGuide = filterInputPost($_POST['filmGuide'], 'filmGuide');
     $description = filterInputPost($_POST['Description'], 'Description');
     $accepted = getTableRecord("SELECT geverifieerd FROM gebruiker WHERE id = ?", "i", array($userId)   );
     executeQuery("INSERT INTO film (gebruiker_id, titel, pad, thumbnail_pad, geaccepteerd, beschrijving, kijkwijzer_leeftijd) VALUES (?, ?, ?, ?, ?, ?, ?)", "isssisi", array($userId, $title, $path, $thumbnailPath, $accepted, $description, $ageRating));
     $filmId = getTableRecords("SELECT MAX(id) FROM film");
     $filmIdInput = getTableRecord("SELECT film.id FROM film WHERE id = ?", "i", array($filmId[0]['MAX(id)']));
-    
     foreach($genres as $genre){
-        $genres = filterInputPost($_POST['Category'], 'Category');
+        $genre = filterInputPost($_POST['Category'], 'Category');
         $genreInput = getTableRecord("SELECT genre.id FROM genre WHERE id = ?", "i", array($genre));
-        executeQuery("INSERT INTO genre_film (genre_id, film_id) VALUES (?, ?)", "ii", array($genreInput['id'], $filmIdInput['id']));
+        executeQuery("INSERT INTO genre_film (genre_id, film_id) VALUES (?, ?)", "ii", array($genreInput, $filmIdInput['id']));
     }
     foreach($filmGuides as $filmGuide){
-        $filmGuides = filterInputPost($_POST['filmGuide'], 'filmGuide');
+        $filmGuide = filterInputPost($_POST['filmGuide'], 'filmGuide');
         $filmGuideInput = getTableRecord("SELECT kijkwijzer_geschiktheid.id FROM kijkwijzer_geschiktheid WHERE id = ?", "i", array($filmGuide));
-        executeQuery("INSERT INTO film_kijkwijzer_geschiktheid (kijkwijzer_geschiktheid_id, film_id) VALUES (?, ?)", "ii", array($filmGuideInput['id'], $filmIdInput['id']));
+        executeQuery("INSERT INTO film_kijkwijzer_geschiktheid (kijkwijzer_geschiktheid_id, film_id) VALUES (?, ?)", "ii", array($filmGuideInput, $filmIdInput['id']));
     }
     $message = "Movie uploaded";
     return $message;
