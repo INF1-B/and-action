5a10ab190a6b23d0a41fa169913a6d38816cfac6
Michel - gaming laptop <michel-disbergen@hotmail.com>
2022-01-06 12:38:13 +0100

    forgot an ! :D

--- public/director-upload-movie.php
+++ public/director-upload-movie.php
@@ -64,7 +64,7 @@ if (isset($_POST['upload'])) {
       $filetypemoviemess = "Your movie has to be atleast 20 minutes in order to be uploaded!";
       $movie = FALSE;
     } 
-    else if (filterInputFileSize($_FILES["Movie"]["tmp_name"], 950000 )) { // max 950MB
+    else if (!filterInputFileSize($_FILES["Movie"]["tmp_name"], 950000 )) { // max 950MB
       $filetypemoviemess = "Your movie exceeds the max upload file size! Max 950MB is allowed";
       $movie = FALSE;
     }
